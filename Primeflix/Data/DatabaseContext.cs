e5b2a39e072f708b5fdd25a62d1019edb655dd5a
Sophie Wenkin <swenkin@neuromedia.io>
2022-10-02 20:26:56 +0200

    ISL-4 Testing database requests with new tables

---
+++ Primeflix/Data/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Primeflix.Models;
+
+namespace Primeflix.Data
+{
+    // This file is used to interact with the database
+
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        { }
+
+        public DbSet<Product> Product { get; set; }
+
+    }
+}
--- Primeflix/Models/Product.cs
+++ Primeflix/Models/Product.cs
@@ -1,12 +1,19 @@
-﻿namespace Primeflix.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Primeflix.Models
 {
     public class Product
     {
+        [Column("product_id")]
         public int Id { get; set; }
-        public string Name { get; set; }
-        public double Price { get; set; }
-        public string Description { get; set; }
+        [Column("original_title")]
+        public string Title { get; set; }
+        [Column("released_date")]
+        public DateTime ReleaseDate { get; set; }
+        public TimeSpan Duration { get; set; }
         public int Stock { get; set; }
-
+        public int Rating { get; set; }
+        public string Format { get; set; }
+        public string Description { get; set; }
     }   
 }
