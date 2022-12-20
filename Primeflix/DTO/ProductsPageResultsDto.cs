dbdcd9f4c16fb60fe234f2a39c80e92051dc442b
Sophie Wenkin <swenkin@neuromedia.io>
2022-12-20 15:12:00 +0100

    ISL-34 Adds pagination to product search and product filter

---
+++ Primeflix/DTO/ProductsPageResultsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Primeflix.DTO
+{
+    public class ProductsPageResultsDto
+    {
+        public List<ProductDto> Products { get; set; }
+        public int TotalPages { get; set; }
+        public int CurrentPage { get; set; }
+    }
+}
